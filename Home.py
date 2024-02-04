55297e1a8a13c3070d438d486333b0afd49183cb
Ninaad Kalla <ninaadkalla@gmail.com>
2024-02-04 11:59:39 -0500

    fixed issues with page refreshing on heatmap button click

--- Home.py
+++ Home.py
@@ -24,11 +24,12 @@ from gseapy.plot import gseaplot
 st.set_page_config(page_title=None,
                    page_icon="🌱",
                    layout="centered",
-                   initial_sidebar_state="expanded",
+                   initial_sidebar_state="auto",
                    menu_items=None)
 
 ### Functions ###
 
+@st.cache_data
 def preprocess_datasets(countData, colData):
 
     # Preprocess countData to pyDESEQ2 format
@@ -49,99 +50,91 @@ def preprocess_datasets(countData, colData):
 
     return countData, colData
 
+@st.cache_data
+def perform_diff_analysis(countData, colData):
 
+    inference = DefaultInference(n_cpus=8)
 
+    dds = DeseqDataSet(
+        counts=countData,
+        metadata=colData,
+        design_factors="condition",
+        refit_cooks=True,
+        inference=inference,
+    )
+
+    dds.deseq2()
+    stat_res = DeseqStats(dds, inference=inference)
+    stat_res.summary()
+
+    return stat_res.results_df, dds
 
 ### Main ###
 
+if 'data_loaded' not in st.session_state:
+    st.session_state.data_loaded = False
+
+if 'analysis_done' not in st.session_state:
+    st.session_state.analysis_done = False
+
 if __name__ == "__main__":
 
-    file = uploaded_file = st.file_uploader("Upload a .csv file",
-                                            type=['csv'],
-                                            accept_multiple_files=False,
-                                           )
-    st.session_state['file'] = file
+    if not st.session_state['data_loaded']:
 
+        with st.spinner("Loading data..."):
 
-    # Load Data
+            dir = os.path.join('data', 'E-GEOD-60052-raw-counts.tsv') # countData
+            df = pd.read_csv(dir, sep='\t')
+            st.session_state.df = df
 
-    with st.spinner("Loading data..."):
+            meta_dir = os.path.join('data', 'E-GEOD-60052-experiment-design.tsv') #colData
+            meta_df = pd.read_csv(meta_dir, sep='\t')
+            st.session_state.meta_df = meta_df
 
-        dir = os.path.join('data', 'E-GEOD-60052.csv')
-        df = pd.read_csv(dir, index_col=0)
+        with st.spinner("Preprocessing data..."):
 
-    # Data Preprocessing
-
-    with st.spinner("Preprocessing data..."):
-
-        df = df.loc[:,~df.columns.duplicated()].copy()
-        df = df[df.columns[df.sum(axis=0) >= 10]]
-        st.session_state['df'] = df
-
-        meta_df = df['condition'].reset_index().rename(columns={'index':'sample'})
-        meta_df = meta_df.set_index('sample').rename_axis(None)
-        meta_df
-        st.session_state['meta_df'] = meta_df
+            countData, colData = preprocess_datasets(df, meta_df)
+            st.session_state.countData = countData
+            st.session_state.colData = colData
+            st.session_state.data_loaded = True
 
     # Differential Analysis
+    if not st.session_state['analysis_done']:
+        with st.spinner("Performing differential analysis..."):
 
-    with st.spinner("Perfoming differential analysis..."):
-        inference = DefaultInference(n_cpus=8)
+            res, dds = perform_diff_analysis(st.session_state['countData'], st.session_state['colData'])
+            st.session_state.res = res
+            st.session_state.dds = dds
+            st.session_state.analysis_done = True
 
-        dds = DeseqDataSet(
-            counts=df,
-            metadata=meta_df,
-            design_factors="condition",
-            refit_cooks=True,
-            inference=inference,
-        )
-
-        dds.deseq2()
-
-        stat_res = DeseqStats(dds, inference=inference)
-        stat_res.summary()
-        res = stat_res.results_df
-        st.session_state['res'] = res
-
-        st.write(res)
-
+    # Input parameters for generating the heatmap
     col1, col2, col3 = st.columns(3)
-
     with col1:
         padj = st.number_input("padj", value=0.05)
-        st.session_state['padj'] = padj
     with col2:
         log2FoldChange = st.number_input("log2FoldChange", value = 0.05)
-        st.session_state['log2FoldChange'] = log2FoldChange
-
     with col3:
         baseMean = st.number_input("base mean", value=20)
-        st.session_state['baseMean'] = baseMean
-
-
     numGenes = st.number_input("Number of overexpressed and underexpressed genes to keep:", value=50)
-    st.session_state['numGenes'] = numGenes
 
     if st.button("Generate Heatmap"):
 
         with st.spinner("Generating heatmap..."):
 
-            res = res[(res['padj'] < padj)]
-            res = res[(abs(res['log2FoldChange']) > log2FoldChange)]
-            res = res[(res['baseMean'] > baseMean)]
-            res.sort_values(by=['log2FoldChange'], ascending=False, inplace=True)
+            filtered_res = st.session_state.res[(st.session_state.res['padj'] < padj) &
+                                            (abs(st.session_state.res['log2FoldChange']) > log2FoldChange) &
+                                            (st.session_state.res['baseMean'] > baseMean)]
 
-            top_res = pd.concat([res.head(numGenes), res.tail(numGenes)])
+            filtered_res.sort_values(by=['log2FoldChange'], ascending=False, inplace=True)
 
-            dds.layers['log1p'] = np.log1p(dds.layers['normed_counts'])
-            dds_sigs = dds[:, top_res.index]
+            top_res = pd.concat([filtered_res.head(numGenes), filtered_res.tail(numGenes)])
+
+            st.session_state.dds.layers['log1p'] = np.log1p(st.session_state.dds.layers['normed_counts'])
+
+            dds_sigs = st.session_state.dds[:, top_res.index]
 
             diffexpr_df = pd.DataFrame(dds_sigs.layers['log1p'].T,
-                            index=dds_sigs.var_names,
-                            columns=dds_sigs.obs_names)
+                                    index=dds_sigs.var_names,
+                                    columns=dds_sigs.obs_names)
 
-            st.pyplot(sns.clustermap(diffexpr_df,
-                                    z_score = 0,
-                                    cmap='RdBu_r'
-                                    )
-                    )
+            st.pyplot(sns.clustermap(diffexpr_df, z_score=0, cmap='RdBu_r'))
